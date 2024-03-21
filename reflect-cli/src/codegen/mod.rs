fc3432d8fabffeff3c23268527cb48bc2915b4b6
Andrey Konstantinov <andrey@partly.com>
2024-03-21 03:33:18 +0000

    codegen for typescript without some falgs support

--- reflect-cli/src/codegen/mod.rs
+++ reflect-cli/src/codegen/mod.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use anyhow::Context;
 
 mod typescript;
@@ -10,11 +12,18 @@ pub fn generate(
     let schema_path = schema.unwrap_or(std::path::PathBuf::from("reflectapi.json"));
     let schema_as_json = std::fs::read_to_string(schema_path.clone())
         .context(format!("Failed to read schema file: {:?}", schema_path))?;
-    let schema: reflect::Schema = serde_json::from_str(&schema_as_json)
+    let schema: reflect::EndpointSchema = serde_json::from_str(&schema_as_json)
         .context("Failed to parse schema file as JSON into reflect::Schema object")?;
     match language {
         crate::Language::Typescript => {
-            typescript::generate(schema, output)?;
+            let generated_code = typescript::generate(schema)?;
+            let output = output.unwrap_or_else(|| std::path::PathBuf::from("./"));
+            let output = output.join("index.ts");
+            let mut file = std::fs::File::create(output.clone())
+                .context(format!("Failed to create file: {:?}", output))?;
+            println!("{}", generated_code);
+            file.write(generated_code.as_bytes())
+                .context(format!("Failed to write to file: {:?}", output))?;
         }
     }
 
