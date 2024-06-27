5dc6eb22104220c08be37d80f93a030006065ac5
Andrey Konstantinov <andrey@partly.com>
2024-06-27 07:11:33 +0000

    fix reflectapi annotation for chrono datetime

--- reflectapi/src/impls/chrono.rs
+++ reflectapi/src/impls/chrono.rs
@@ -1,13 +1,10 @@
-fn reflectapi_date_time(schema: &mut crate::Typespace, timezone: &str) -> String {
+fn reflectapi_date_time(schema: &mut crate::Typespace) -> String {
     let type_name = "chrono::DateTime";
-    if schema.reserve_type(&type_name) {
+    if schema.reserve_type(type_name) {
         let type_def = crate::Primitive::new(
             type_name.into(),
-            format!(
-                "DateTime at {} timezone",
-                timezone.split("::").last().unwrap()
-            ),
-            vec![timezone.into()],
+            "DateTime at a given timezone".into(),
+            vec!["Tz".into()],
             Some("std::string::String".into()),
         );
         schema.insert_type(type_def.into());
@@ -16,32 +13,50 @@ fn reflectapi_date_time(schema: &mut crate::Typespace, timezone: &str) -> String
 }
 impl crate::Input for chrono::DateTime<chrono::Utc> {
     fn reflectapi_input_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::Utc").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::Utc".into()],
+        )
     }
 }
 impl crate::Output for chrono::DateTime<chrono::Utc> {
     fn reflectapi_output_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::Utc").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::Utc".into()],
+        )
     }
 }
 impl crate::Input for chrono::DateTime<chrono::Local> {
     fn reflectapi_input_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::Local").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::Local".into()],
+        )
     }
 }
 impl crate::Output for chrono::DateTime<chrono::Local> {
     fn reflectapi_output_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::Local").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::Local".into()],
+        )
     }
 }
 impl crate::Input for chrono::DateTime<chrono::FixedOffset> {
     fn reflectapi_input_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::FixedOffset").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::FixedOffset".into()],
+        )
     }
 }
 impl crate::Output for chrono::DateTime<chrono::FixedOffset> {
     fn reflectapi_output_type(schema: &mut crate::Typespace) -> crate::TypeReference {
-        reflectapi_date_time(schema, "chrono::FixedOffset").into()
+        crate::TypeReference::new(
+            reflectapi_date_time(schema),
+            vec!["chrono::FixedOffset".into()],
+        )
     }
 }
 
