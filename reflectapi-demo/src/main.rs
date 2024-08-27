ad88b122351aeab5b628181d378f9aa53d75fd14
Andy Yu <andyyu2004@gmail.com>
2024-08-27 17:05:46 +1200

    add redoc documentation to demo

--- reflectapi-demo/src/main.rs
+++ reflectapi-demo/src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     let builder = reflectapi_demo::builder();
-    let (schema, routers) = match builder.build() {
+    let (schema, mut routers) = match builder.build() {
         Ok((schema, routers)) => (schema, routers),
         Err(errors) => {
             for error in errors {
@@ -10,6 +10,60 @@ async fn main() {
             return;
         }
     };
+
+    let codegen_config = reflectapi::codegen::Config::default();
+    let openapi_schema_json: &'static str = Box::leak(
+        reflectapi::codegen::openapi::generate(schema.clone(), &codegen_config)
+            .unwrap()
+            .into_boxed_str(),
+    );
+
+    routers.extend([
+        reflectapi::Router {
+            name: "openapi".to_string(),
+            handlers: vec![reflectapi::Handler {
+                name: "openapi.json".to_string(),
+                path: "".to_string(),
+                readonly: true,
+                input_headers: vec![],
+                callback: std::sync::Arc::new(move |_state, _body| {
+                    Box::pin(async move {
+                        reflectapi::HandlerOutput {
+                            code: http::StatusCode::OK,
+                            body: openapi_schema_json.into(),
+                            headers: http::HeaderMap::from_iter([(
+                                http::HeaderName::from_static("content-type"),
+                                "application/json".parse().unwrap(),
+                            )]),
+                        }
+                    })
+                }),
+            }],
+        },
+        reflectapi::Router {
+            name: "openapi".to_string(),
+            handlers: vec![reflectapi::Handler {
+                name: "doc".to_string(),
+                path: "".to_string(),
+                readonly: true,
+                input_headers: vec![],
+                callback: std::sync::Arc::new(|_state, _body| {
+                    Box::pin(async move {
+                        reflectapi::HandlerOutput {
+                            code: http::StatusCode::OK,
+                            body: include_bytes!("./redoc.html")[..].into(),
+                            headers: {
+                                http::HeaderMap::from_iter([(
+                                    http::HeaderName::from_static("content-type"),
+                                    "text/html".parse().unwrap(),
+                                )])
+                            },
+                        }
+                    })
+                }),
+            }],
+        },
+    ]);
 
     // write reflect schema to a file
     tokio::fs::write(
@@ -28,5 +82,6 @@ async fn main() {
         r.layer(tower_http::trace::TraceLayer::new_for_http())
     });
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    eprintln!("Listening on http://0.0.0.0:3000");
     axum::serve(listener, axum_app).await.unwrap();
 }
