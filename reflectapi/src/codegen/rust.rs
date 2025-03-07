ae71778698ae077d44e4d1f13637f8ebe78bafcd
Josiah Bull <jo@partly.com>
2025-03-07 17:39:00 +1300

    chore: ran formatting.

--- reflectapi/src/codegen/rust.rs
+++ reflectapi/src/codegen/rust.rs
@@ -501,7 +501,6 @@ pub struct {{ name }} {{ self.render_brackets().0 }}
             if self.optional {
                 serde_attrs.push("default = \"Default::default\"".into());
 
-
                 // this one is important to not serialize undefined values
                 // as this is the special built-in type which allows to differentiate between undefined and null
                 if self.type_.starts_with("reflectapi::Option<") {
