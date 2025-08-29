29088a1a74ad4b5daed433b6cda8562e684477c9
Andrey Konstantinov <andrey@partly.com>
2025-08-29 11:50:31 +0000

    add root level lib code sample

--- reflectapi/src/lib.rs
+++ reflectapi/src/lib.rs
@@ -6,12 +6,78 @@
 //! Define your server
 //!
 //! ```rust
-//! asdasdasd
+//!
+//! #[derive(Debug)]
+//! pub struct AppState {
+//!     pub books: Vec<Book>,
+//! }
+//!
+//! async fn books_list(
+//!     state: std::sync::Arc<AppState>,
+//!     input: proto::Cursor,
+//!     headers: proto::Authorization,
+//! ) -> Result<proto::Items<Book>, proto::BooksListError> {
+//!     unimplemented!("just a demo of API signature")
+//! }
+//!
+//! pub fn builder() -> reflectapi::Builder<Arc<AppState>> {
+//!     reflectapi::Builder::new()
+//!         .route(books_list, |b| {
+//!             b.name("books.list").description("List all books")
+//!         })
+//! }
+//!
+//! impl Default for AppState {
+//!     fn default() -> Self {
+//!         Self {
+//!             books: vec![Book {
+//!                 isbn: "978-3-16-148410-0".into(),
+//!                 title: "The Catcher in the Rye".into(),
+//!             }],
+//!         }
+//!     }
+//! }
+//!
+//! #[derive(
+//!    Debug, Clone, serde::Serialize, serde::Deserialize, reflectapi::Input, reflectapi::Output,
+//! )]
+//! pub struct Book {
+//!     /// ISBN - identity
+//!     pub isbn: String,
+//!     /// Title
+//!     pub title: String,
+//! }
+//!
+//! pub mod proto {
+//!     #[derive(serde::Deserialize, reflectapi::Input)]
+//!     pub struct Authorization {
+//!         pub authorization: String,
+//!     }
+//!
+//!     #[derive(serde::Deserialize, reflectapi::Input)]
+//!     pub struct Cursor {
+//!         #[serde(default)]
+//!         pub cursor: Option<String>,
+//!         #[serde(default)]
+//!         pub limit: Option<u32>,
+//!     }
+//!     #[derive(serde::Serialize, reflectapi::Output)]
+//!     pub struct Items<T> {
+//!         pub items: Vec<T>,
+//!         pub pagination: Pagination,
+//!     }
+//!
+//!     #[derive(serde::Serialize, reflectapi::Output)]
+//!     pub struct Pagination {
+//!         pub next_cursor: Option<String>,
+//!         pub prev_cursor: Option<String>,
+//!     }
+//! }
 //!
 //! ```
-//! 
+//!
 //! Now use it from Typescript (one of the languages supported by the codegen):
-//! 
+//!
 //! ```typescript
 //! ```
 //!
