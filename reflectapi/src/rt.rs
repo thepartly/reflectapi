bedf7e6e93f4599d659c9613eaa9991407009472
Andy Yu <andyyu2004@gmail.com>
2026-02-10 13:05:02 +1300

    format

--- reflectapi/src/rt.rs
+++ reflectapi/src/rt.rs
@@ -1,8 +1,7 @@
 pub use url::{ParseError as UrlParseError, Url};
 
 pub fn error_to_string<T: serde::Serialize>(error: &T) -> String {
-    serde_json::to_string(error)
-        .unwrap_or_else(|e| format!("Failed to serialize error: {e}"))
+    serde_json::to_string(error).unwrap_or_else(|e| format!("Failed to serialize error: {e}"))
 }
 
 pub trait Client {
